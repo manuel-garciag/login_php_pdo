f663939ee5de51d6ebff0303d6374602fdb2b0fa
Manuel Garcia <hola@manuelgarcia.space>
2021-09-21 21:58:34 -0500

    vista del login y validacion

---
+++ vista/login.php
@@ -0,0 +1,37 @@
+<?php
+include 'partials/head.php';
+include 'partials/menu.php';
+?>
+<div class="container">
+
+    <div class="starter-template">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form action="validarCode.php" method="POST" role="form">
+                            <legend>Iniciar Sesión</legend>
+
+                            <div class="form-group">
+                                <label for="usuario">Usuario</label>
+                                <input class="form-control" type="text" name="txtUsuario" id="usuario" placeholder="usuario" required autofocus>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input class="form-control" type="password" name="txtPassword" id="password" placeholder="****" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-success">Ingresar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div><!-- /.container -->
+
+<?php
+include 'partials/footer.php';
+?>
--- vista/validarCode.php
+++ vista/validarCode.php
@@ -2,4 +2,19 @@
 
 include '../controlador/usuarioControlador.php';
 
-echo UsuarioControlador::login("manuel_garcia", "1234");
+$resultado = array();
+
+if (isset($_POST['txtUsuario']) && isset($_POST['txtPassword'])) {
+
+    $txtUsuario = $_POST['txtUsuario'];
+    $txtPassword = $_POST['txtPassword'];
+
+    $resultado = array('estado' => true);
+
+    if (UsuarioControlador::login($txtUsuario, $txtPassword)) {
+        return print(json_encode($resultado));
+    }
+}
+
+$resultado = array('estado' => false);
+return print(json_encode($resultado));
